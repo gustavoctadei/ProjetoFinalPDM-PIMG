cfef1f9c14d00cb70c36988274b1fd5ace74d949
Gustavo Tadei <gustavo.tadei@hotmail.com>
2023-06-28 22:51:46 -0400

    att

--- backend/main.py
+++ backend/main.py
@@ -13,13 +13,16 @@ def handle_request():
     imagefile = flask.request.files['image']
     filename = werkzeug.utils.secure_filename(imagefile.filename)
     imagefile.save(filename)
+    print('Imagem recebida')
 
     img_out = carregar_imagem(imagefile.filename)
     os.remove(imagefile.filename)
+    print('Imagem carregada. Iniciando processamento.')
 
     img_out = filtro_bilateral(img_out)
     #img_out = filtro_bilateral_opencv(img_out) #Utilizando o Filtro Bilateral da Biblioteca OpenCV
     cv2.imwrite('img_out.jpg', img_out)
+    print('Imagem processada. Respondendo a requisicao')
     
     return send_file('img_out.jpg', mimetype='image/jpg')
 
